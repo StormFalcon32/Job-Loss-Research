b1f07793fe1123ef94cf0472f34d42ff47383cbc
StormFalcon32 <benchen9708@gmail.com>
2020-11-14 11:33:02 -0500

    Implemented LSH

--- RemoveSimilar.py
+++ RemoveSimilar.py
@@ -1,14 +1,9 @@
 import json
-import multiprocessing as mp
 from pathlib import Path
 
-import Levenshtein
+from datasketch import MinHash, MinHashLSH
+from nltk import ngrams
 
-def check_ratio(similar_removed, try_tweet):
-    for tweet in similar_removed:
-        if Levenshtein.ratio(try_tweet, tweet[1]) > 0.9:
-            return 1
-    return 0
 
 def main():
     path = Path('C:/Data/Python/JobLoss')
@@ -17,18 +12,30 @@ def main():
         data = json.load(f)
         for tweet in data:
             orig_data.append(tweet[2])
-    similar_removed = []
-    similar_removed.append((0, orig_data[0]))
-    for i in range(1, len(orig_data)):
-        print('%s / %s' % (i, len(orig_data)))
-        pool = mp.Pool(mp.cpu_count())
-        too_similar = 0
-        try_tweet = orig_data[i]
-        too_similar = pool.apply(check_ratio, args=(similar_removed, try_tweet))
-        if not too_similar:
-            similar_removed.append((i, try_tweet))
-        pool.close()
-    final = [data[ind[0]] for ind in similar_removed]
+#     orig_data = ['minhash is a probabilistic data structure for estimating the similarity between datasets',
+#   'finhash dis fa frobabilistic fata ftructure for festimating the fimilarity fetween fatasets',
+#   'minhash is a data structure for estimating the similarity between datasets',
+#   'weights controls the relative importance between minizing false positive',
+#   'wfights cfntrols the rflative ifportance befween minizing fflse posftive',
+#   'unique'
+# ]
+    markers = [0 for _ in range(len(orig_data))]
+    lsh = MinHashLSH(threshold=0.4, num_perm=128)
+    minhashes = {}
+    for c, i in enumerate(orig_data):
+        print(c)
+        minhash = MinHash(num_perm=128)
+        for d in ngrams(i, 3):
+            minhash.update(''.join(d).encode('utf-8'))
+        lsh.insert(c, minhash)
+        minhashes[c] = minhash
+    for i in range(len(minhashes.keys())):
+        result = lsh.query(minhashes[i])
+        markers[i] = 1
+        for j in result:
+            if markers[j] != 1:
+                markers[j] = 2
+    final = [data[ind] for ind, val in enumerate(markers) if val != 2]
     with open(path / 'ProcessedSimilarRemoved.json', 'w') as f:
             json.dump(final, f)
 
